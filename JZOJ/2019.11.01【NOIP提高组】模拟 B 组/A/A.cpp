b7ca0f048c6a354977070c88e53ca35631ff6760
MXR128 <M_GT_@outlook.com>
2019-11-01 16:39:23 +0800

    Revert "Revert "2019.11.01【NOIP提高组】模拟 B 组_250""
    This reverts commit ffbd5cfdc8da96098e662a58e206a3e9f817f6b7.

--- JZOJ/2019.11.01【NOIP提高组】模拟 B 组/A/A.cpp
+++ JZOJ/2019.11.01【NOIP提高组】模拟 B 组/A/A.cpp
@@ -6,13 +6,14 @@
 int n, a[1000020], ans = 0;
 
 int main() {
-	freopen("A.in", "r", stdin);
+	freopen("team0.in", "r", stdin);
 	scanf("%d", &n);
 	for (int i = 1; i <= n; ++i)
 		scanf("%d", &a[i]);
 	std::sort(&a[1], &a[n + 1]);
-	for (int i = n; i > 0; i -= a[i])
-		++ans;
+	for (int i = 1, cnt = 0; i <= n; ++i)
+		if (++cnt == a[i])
+			++ans, cnt = 0;
 	printf("%d", ans);
 	return 0;
 }
