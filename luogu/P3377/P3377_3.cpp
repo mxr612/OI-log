a50f1e8debd266b3fdd53e3378fb9bfb18de184c
MXR128 <M_GT_@outlook.com>
2020-03-16 20:03:07 +0800

    可并堆
    斜堆
    随机堆
    左偏树

---
+++ luogu/P3377/P3377_3.cpp
@@ -0,0 +1,96 @@
+/**
+ * P3377
+ * 可并堆
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+
+class __heap {
+
+	int cnt = 0;
+
+	struct __node {
+		int v, dis;
+		bool del;
+		__node *ls, *rs, *fa;
+	}*node[100020] = {NULL};
+
+	__node *_get_(__node *x) {
+		if (x->fa != x)
+			x->fa = _get_(x->fa);
+		return x->fa;
+	}
+
+	__node *_merge_(__node *x, __node *y) {
+		if (x == NULL)return y;
+		if (y == NULL)return x;
+		if (x->v > y->v)std::swap(x, y);
+
+		x->rs = _merge_(x->rs, y);
+
+		// 斜堆
+		std::swap(x->ls, x->rs);
+
+		// 随机堆
+		// if (rand() & 1)std::swap(x->ls, x->rs);
+
+		// 左偏树
+		// if (x->ls == NULL || (x->rs != NULL && x->ls->dis < x->rs->dis))std::swap(x->ls, x->rs);
+		// if (x->rs != NULL)x->dis = x->rs->dis + 1;
+
+		return x;
+	}
+
+public:
+
+	inline void _build(register int X) {
+		node[++cnt] = (__node *)calloc(sizeof(__node), 1);
+		node[cnt]->v = X;
+		node[cnt]->fa = node[cnt];
+	}
+
+	inline void _merge(register int X, register int Y) {
+		if (node[X]->del || node[Y]->del)return ;
+		register __node *x = _get_(node[X]), *y = _get_(node[Y]);
+		if (x == y)return ;
+		x->fa = y->fa = _merge_(x, y);
+	}
+
+	inline int _top_pop(register int X) {
+		if (node[X]->del)return -1;
+		register __node *x = _get_(node[X]);
+		if (x->del)return -1;
+		x->del = true;
+		x->fa = _merge_(x->ls, x->rs);
+		if (x->ls != NULL)x->ls->fa = x->fa;
+		if (x->rs != NULL)x->rs->fa = x->fa;
+		return x->v;
+	}
+
+} heap;
+
+signed main() {
+
+	freopen("P3377_5.in", "r", stdin);
+
+	int n, m, o, x, y;
+
+	scanf("%d%d", &n, &m);
+
+	for (register int i = 0; i < n; ++i)
+		scanf("%d", &x), heap._build(x);
+
+	for (register int i = 0; i < m; ++i) {
+		scanf("%d", &o);
+		if (o & 1)
+			scanf("%d%d", &x, &y), heap._merge(x, y);
+		else
+			scanf("%d", &x), printf("%d\n", heap._top_pop(x));
+	}
+
+	return 0;
+
+}
