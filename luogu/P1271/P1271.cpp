558346180d0f472f834e2e9fe3790688639a5d50
mxr128 <m_gt_@outlook.com>
2020-04-10 20:17:25 +0800

    luogu_P1271_计数排序

---
+++ luogu/P1271/P1271.cpp
@@ -0,0 +1,31 @@
+/**
+ * P1271
+ */
+
+#include <stdio.h>
+
+#include <algorithm>
+
+#define MXN (1020)
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("P1271.in", "r", stdin);
+#endif
+
+    int n, m, c, cnt[MXN] = {0};
+
+    scanf("%d%d", &n, &m);
+
+    for (register int i = 0; i < m; ++i)
+        scanf("%d", &c), ++cnt[c];
+
+    for (register int i = n - 1; i > 0; --i)
+        cnt[i] += cnt[i + 1];
+
+    for (register int i = 1; i <= n; ++i)
+        while (cnt[i]-- > cnt[i + 1])
+            printf("%d ", i);
+
+    return 0;
+}
