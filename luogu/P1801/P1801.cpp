5832122c032a29792e027a548b52246737b2b391
mxr128 <m_gt_@outlook.com>
2020-04-15 03:24:22 +0800

    luogu_P1801_heap手写堆迭代版

--- luogu/P1801/P1801.cpp
+++ luogu/P1801/P1801.cpp
@@ -1,6 +1,7 @@
 /**
  * luogu_P1801
  * heap
+ * 手写堆优化
  */
 
 #include <stdio.h>
@@ -17,51 +18,58 @@ inline void _swap(register int& x, register int& y) {
 class __Bheap {
     int arr[MXN], cnt = 0;
 
-    void _down(int x) {
-        if (x << 1 >= cnt)
-            return;
-        if (arr[x] < arr[x << 1])
-            _swap(arr[x], arr[x << 1]), _down(x << 1);
-        if (arr[x] < arr[(x << 1) + 1])
-            _swap(arr[x], arr[(x << 1) + 1]), _down((x << 1) + 1);
+    inline void _down(register int x) {
+        register int l = x, r;
+        while (x << 1 < cnt && x >= l)
+            if (arr[l = x << 1] < arr[r = (x << 1) + 1]) {
+                if (arr[x] < arr[r])
+                    _swap(arr[x], arr[r]), x = r;
+            } else if (arr[x] < arr[l])
+                _swap(arr[x], arr[l]), x = l;
+        if (x << 1 == cnt && arr[x] < arr[cnt])
+            _swap(arr[x], arr[cnt]);
+    }
+
+    inline void _up(register int x) {
+        while (x > 1 && arr[x] > arr[x >> 1])
+            _swap(arr[x], arr[x >> 1]), _down(x), x >>= 1;
     }
 
    public:
-    inline void _push(register int x) {
-        arr[++cnt] = x;
-        for (register int i = cnt; i > 1 && arr[i >> 1] < arr[i]; i >>= 1)
-            _swap(arr[i], arr[i >> 1]), _down(i);
-    }
+    inline void _push(register int x) { arr[++cnt] = x, _up(cnt); }
     inline void _pop() { _swap(arr[1], arr[cnt--]), _down(1); }
     inline int _top() { return arr[1]; }
-    inline int _size() { return cnt; }
 } Bheap;
 
 class __Sheap {
     int arr[MXN], cnt = 0;
 
-    void _down(int x) {
-        if (x << 1 >= cnt)
-            return;
-        if (arr[x] > arr[x << 1])
-            _swap(arr[x], arr[x << 1]), _down(x << 1);
-        if (arr[x] > arr[(x << 1) + 1])
-            _swap(arr[x], arr[(x << 1) + 1]), _down((x << 1) + 1);
+    inline void _down(register int x) {
+        register int l = x, r;
+        while (x << 1 < cnt && x >= l)
+            if (arr[l = x << 1] > arr[r = (x << 1) + 1]) {
+                if (arr[x] > arr[r])
+                    _swap(arr[x], arr[r]), x = r;
+            } else if (arr[x] > arr[l])
+                _swap(arr[x], arr[l]), x = l;
+        if (x << 1 == cnt && arr[x] > arr[cnt])
+            _swap(arr[x], arr[cnt]);
+    }
+
+    inline void _up(register int x) {
+        while (x > 1 && arr[x] < arr[x >> 1])
+            _swap(arr[x], arr[x >> 1]), _down(x), x >>= 1;
     }
 
    public:
-    inline void _push(register int x) {
-        arr[++cnt] = x;
-        for (register int i = cnt; i > 1 && arr[i >> 1] > arr[i]; i >>= 1)
-            _swap(arr[i], arr[i >> 1]), _down(i);
-    }
+    inline void _push(register int x) { arr[++cnt] = x, _up(cnt); }
     inline void _pop() { _swap(arr[1], arr[cnt--]), _down(1); }
     inline int _top() { return arr[1]; }
 } Sheap;
 
 signed main() {
 #ifndef ONLINE_JUDGE
-    freopen("P1801_1.in", "r", stdin);
+    freopen("P1801_11.in", "r", stdin);
     // freopen("P1801.out", "w", stdout);
 #endif
 
@@ -72,19 +80,10 @@ signed main() {
 
     for (register int i = 1, j = 1; i <= n; ++i) {
         scanf("%d", &u);
-
-        while (j <= u) {
-            if (!Bheap._size() || add[j] <= Bheap._top()) {
-                Bheap._push(add[j++]);
-                while (Bheap._size() > i)
-                    Sheap._push(Bheap._top()), Bheap._pop();
-            } else
-                Sheap._push(add[j++]);
-        }
-        while (Bheap._size() < i)
-            Bheap._push(Sheap._top()), Sheap._pop();
-
-        printf("%d\n", Bheap._top());
+        while (j <= u)
+            Bheap._push(add[j++]), Sheap._push(Bheap._top()), Bheap._pop();
+        printf("%d\n", Sheap._top());
+        Bheap._push(Sheap._top()), Sheap._pop();
     }
 
     return 0;
