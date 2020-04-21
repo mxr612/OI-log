416cb8b7eebb8c19bd7f98183474d844409dbb50
MXR612 <MXR612@Lion.local>
2020-04-21 14:59:18 +0800

    Mac settings

--- test/HW/HW.c
+++ test/HW/HW.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 
 int main() {
-	// freopen("HW.out", "w", stdout);
-	printf("Hello, world!\n");
-	int a,b;
-	scanf("%d%d",&a,&b);
-	printf("%d",a+b);
-	return 0;
+    // freopen("HW.out", "w", stdout);
+    freopen("HW.in", "r", stdin);
+    printf("Hello, world!\n");
+    int a, b;
+    scanf("%d%d", &a, &b);
+    printf("%d", a + b);
+    return 0;
 }
