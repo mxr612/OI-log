1113782ebd6388960c4dcb4b4ce8ed49a1827888
MXR612 <mxxr612@gmail.com>
2020-04-26 12:03:56 +0800

    NOI Online T2 太卡常辣

--- CCF/NOI_Online/002_2020_4_25/TG/B/sequence.cpp
+++ CCF/NOI_Online/002_2020_4_25/TG/B/sequence.cpp
@@ -25,75 +25,65 @@ bool cmp2(LSH x, LSH y) {
     return x.d < y.d;
 }
 
-class LST {
-    struct node {
-        int l, r, s, t, w;
-        long long p;
-        node *ls, *rs;
-    }* root = NULL;
+struct node {
+    int l, r, s,t, w;
+    long long p;
+    node *ls, *rs;
+}* root = NULL;
 
-    void build(node*& x, int l, int r) {
-        if (r < l)
-            return;
-        if (x == NULL)
-            x = (node*)calloc(sizeof(node), 1);
-        x->l = l, x->r = r, x->s = r - l + 1;
-        if (l < r)
-            build(x->ls, l, (l + r) >> 1), build(x->rs, ((l + r) >> 1) + 1, r);
-    }
+void build(node*& x, int l, int r) {
+    if (r < l)
+        return;
+    if (x == NULL)
+        x = (node*)calloc(sizeof(node), 1);
+    x->l = l, x->r = r, x->s = r - l + 1;
+    if (l < r)
+        build(x->ls, l, (l + r) >> 1), build(x->rs, ((l + r) >> 1) + 1, r);
+}
 
-    void pow(node* x, long long v) {
-        x->p = (x->p + (v * x->w << 1) + x->s * v * v) % MOD, x->w += v * x->s;
-    }
-
-    void push(node* x) {
-        if (x != NULL && x->ls != NULL) {
-            pow(x->ls, x->t), x->ls->t += x->t;
-            pow(x->rs, x->t), x->rs->t += x->t;
+void modify(node* x, int l, int r) {
+    if (x == NULL || x->r < l || r < x->l)
+        return;
+    if (l <= x->l && x->r <= r)
+        x->p = (x->p + (x->w << 1) + x->s) % MOD, x->w += x->s, ++x->t;
+    else {
+        if (x->t > 0) {
+            register node* xy = x->ls;
+            long long t = x->t;
+            xy->p = (xy->p + (t * xy->w << 1) + xy->s * t * t) % MOD,
+            xy->w += t * xy->s, xy->t += t;
+            xy = x->rs;
+            xy->p = (xy->p + (t * xy->w << 1) + xy->s * t * t) % MOD,
+            xy->w += t * xy->s, xy->t += t;
             x->t = 0;
         }
+        modify(x->ls, l, r);
+        modify(x->rs, l, r);
+        x->p = x->ls->p + x->rs->p;
+        x->w = x->ls->w + x->rs->w;
     }
-
-    void modify(node* x, int l, int r) {
-        if (x == NULL || x->r < l || r < x->l)
-            return;
-        if (l <= x->l && x->r <= r)
-            pow(x, 1), ++x->t;
-        else {
-            push(x);
-            modify(x->ls, l, r);
-            modify(x->rs, l, r);
-            x->p = x->ls->p + x->rs->p;
-            x->w = x->ls->w + x->rs->w;
-        }
-    }
-
-   public:
-    void build() { build(root, 1, N); }
-    void modify(int l, int r) { modify(root, l, r); }
-    long long query() { return root->p; }
-} lst;
+}
 
 signed main() {
     freopen("sequence.in", "r", stdin);
     freopen("sequence.out", "w", stdout);
 
     scanf("%d", &N);
-    for (int i = 1, p; i <= N; ++i)
+    for (register int i = 1, p; i <= N; ++i)
         scanf("%d", &lsh[i].a), lsh[i].d = i;
 
     std::sort(&lsh[1], &lsh[N + 1], cmp1);
-    for (int i = 1, p = 0; i <= N; ++i)
+    for (register int i = 1, p = 0; i <= N; ++i)
         lsh[i].b = (lsh[i].a == lsh[i - 1].a) ? (p) : (++p);
     std::sort(&lsh[1], &lsh[N + 1], cmp2);
-    for (int i = 1; i <= N; ++i)
+    for (register int i = 1; i <= N; ++i)
         A[i] = lsh[i].b;
 
-    lst.build();
+    build(root, 1, N);
 
-    for (int i = 1; i <= N; ++i) {
-        lst.modify((las[A[i]] == 0) ? (1) : (las[A[i]] + 1), i);
-        ans = (ans + lst.query()) % MOD;
+    for (register int i = 1; i <= N; ++i) {
+        modify(root, (las[A[i]] == 0) ? (1) : (las[A[i]] + 1), i);
+        ans = (ans + root->p) % MOD;
         las[A[i]] = i;
     }
 
