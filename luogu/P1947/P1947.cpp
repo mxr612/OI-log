c5ef87aec3ab108ae1497366ee2a99cd9e91ed37
MXR612 <mxxr612@gmail.com>
2020-05-16 23:47:14 +0800

    luoguP1947添加了调试主程序

--- luogu/P1947/P1947.cpp
+++ luogu/P1947/P1947.cpp
@@ -24,3 +24,29 @@ extern "C" int Chtholly(int n, int c) {
         }
     }
 }
+
+#include <cstdio>
+#include <iostream>
+
+int k, cnt;
+
+extern "C" {
+extern int Chtholly(int n, int c);
+
+extern int Seniorious(int x) {
+    const int lim = 3000000;
+    if (++cnt > lim)
+        cnt = lim;
+    return (k < x) ? 1 : ((k == x) ? 0 : -1);
+}
+}
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("P1947.in", "r", stdin);
+#endif
+    int n, c;
+    std::cin >> n >> c >> k;
+    int OvO = Chtholly(n, c);
+    std::cout << OvO << ' ' << cnt << std::endl;
+    return 0;
+}
