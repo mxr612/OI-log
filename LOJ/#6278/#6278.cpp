b131f32b2f15b9cf6fa513594e070ef9d621925e
MXR612 <mxxr612@gmail.com>
2020-05-27 20:11:16 +0800

    LOJ#6278FinallyAccepted

--- LOJ/#6278/#6278.cpp
+++ LOJ/#6278/#6278.cpp
@@ -8,41 +8,42 @@
 
 #include <algorithm>
 
-#define MXN (50020)
+#define MXN (50300)
 
-long long arr[MXN], sor[MXN], tag[MXN] = {0};
-int pos[MXN] = {0};
+long long arr[MXN], sor[MXN], tag[MXN];
+int pos[MXN] = {0}, pol[MXN];
 
 signed main() {
 #ifndef ONLINE_JUDGE
     freopen("a1.in", "r", stdin);
     FILE* ansf = fopen("a1.out", "r");
-    long long anss, cnt = 0;
+    int cnt = 0;
+    long long anss;
 #endif
 
     int n, siz, opt, l, r, ll, rr, mm;
-    long long c, ans, cc;
+    long long c, cc, ans;
 
     scanf("%d", &n), siz = sqrt(n);
 
     for (int i = 1; i <= n; ++i)
-        scanf("%lld", &arr[i]), sor[i] = arr[i], pos[i] = ((i - 1) / siz) + 1;
+        scanf("%lld", &arr[i]), sor[i] = arr[i], pos[i] = (i - 1) / siz + 1;
     pos[n + 1] = pos[n];
     for (int i = 1; i <= n; i += siz)
-        std::sort(&sor[i], &sor[i + siz]);
+        std::sort(&sor[pol[pos[i]] = i], &sor[i + siz]);
 
     for (int i = 0; i < n; ++i) {
         scanf("%d%d%d%lld", &opt, &l, &r, &c);
         if (opt) {
-            c *= c, ans = 0;
-            for (cc = c - tag[pos[l]]; l <= r && pos[l - 1] == pos[l]; l++)
+            ans = 0, c *= c;
+            for (cc = c - tag[pos[l]]; l <= r && pos[l] == pos[l - 1]; ++l)
                 if (arr[l] < cc)
                     ++ans;
-            for (cc = c - tag[pos[r]]; l <= r && pos[r] == pos[r + 1]; r--)
+            for (cc = c - tag[pos[r]]; l <= r && pos[r] == pos[r + 1]; --r)
                 if (arr[r] < cc)
                     ++ans;
             for (; l <= r; l += siz) {
-                cc = c - tag[pos[ll = l]], rr = l + siz - 1;
+                cc = c - tag[pos[l]], ll = l, rr = l + siz;
                 while (ll < rr)
                     if (sor[mm = (ll + rr) >> 1] < cc)
                         ll = mm + 1;
@@ -54,24 +55,24 @@ signed main() {
             ++cnt;
             fscanf(ansf, "%lld", &anss);
             if (ans != anss)
-                printf("%5d %10lld %10lld\n", cnt, anss, ans);
+                printf("%5d %15lld %15lld\n", cnt, anss, ans);
 #else
             printf("%lld\n", ans);
 #endif
         } else {
-            if (pos[l - 1] == pos[l]) {
-                while (l <= r && pos[l - 1] == pos[l])
+            if (l <= r && pos[l] == pos[l - 1]) {
+                while (l <= r && pos[l] == pos[l - 1])
                     arr[l++] += c;
-                for (int j = l - siz; j < l; ++j)
+                for (int j = pol[pos[l - 1]]; j < pol[pos[l - 1]] + siz; ++j)
                     sor[j] = arr[j];
-                std::sort(&sor[l - siz], &sor[l]);
+                std::sort(&sor[pol[pos[l - 1]]], &sor[pol[pos[l - 1]] + siz]);
             }
-            if (pos[r] == pos[r + 1]) {
+            if (l <= r && pos[r] == pos[r + 1]) {
                 while (l <= r && pos[r] == pos[r + 1])
                     arr[r--] += c;
-                for (int j = r + 1; j <= r + siz; ++j)
+                for (int j = pol[pos[r + 1]]; j < pol[pos[r + 1]] + siz; ++j)
                     sor[j] = arr[j];
-                std::sort(&sor[r + 1], &sor[r + siz + 1]);
+                std::sort(&sor[pol[pos[r + 1]]], &sor[pol[pos[r + 1]] + siz]);
             }
             for (; l <= r; l += siz)
                 tag[pos[l]] += c;
