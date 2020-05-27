df2117703e3ecf6f3888f4773ff3e7b296d38423
MXR612 <mxxr612@gmail.com>
2020-05-27 19:09:53 +0800

    LOJ#6278NotAccepted

--- LOJ/#6278/#6278.cpp
+++ LOJ/#6278/#6278.cpp
@@ -11,62 +11,70 @@
 #define MXN (50020)
 
 long long arr[MXN], sor[MXN], tag[MXN] = {0};
-int pos[MXN];
+int pos[MXN] = {0};
 
 signed main() {
 #ifndef ONLINE_JUDGE
-    freopen("#6278.in", "r", stdin);
+    freopen("a1.in", "r", stdin);
+    FILE* ansf = fopen("a1.out", "r");
+    long long anss, cnt = 0;
 #endif
 
-    int n, opt, l, r, siz;
-    long long c, ans;
+    int n, siz, opt, l, r, ll, rr, mm;
+    long long c, ans, cc;
 
-    scanf("%d", &n);
-    siz = sqrt(n);
+    scanf("%d", &n), siz = sqrt(n);
 
     for (int i = 1; i <= n; ++i)
-        scanf("%lld", &arr[i]), sor[i] = arr[i], pos[i] = i / siz;
-
-    for (int i = 1, ed = n - (n % siz); i <= ed; i += siz)
+        scanf("%lld", &arr[i]), sor[i] = arr[i], pos[i] = ((i - 1) / siz) + 1;
+    pos[n + 1] = pos[n];
+    for (int i = 1; i <= n; i += siz)
         std::sort(&sor[i], &sor[i + siz]);
 
     for (int i = 0; i < n; ++i) {
         scanf("%d%d%d%lld", &opt, &l, &r, &c);
         if (opt) {
             c *= c, ans = 0;
-            while (l <= r && pos[l] == pos[l - 1])
-                if (arr[l++] < c)
+            for (cc = c - tag[pos[l]]; l <= r && pos[l - 1] == pos[l]; l++)
+                if (arr[l] < cc)
                     ++ans;
-            while (l <= r && pos[r] == pos[r + 1])
-                if (arr[r--] < c)
+            for (cc = c - tag[pos[r]]; l <= r && pos[r] == pos[r + 1]; r--)
+                if (arr[r] < cc)
                     ++ans;
-            for (int i = l, cc, ll, rr, mm; i <= r; i += siz) {
-                cc = c - tag[pos[i]], ll = i, rr = i + siz - 1;
+            for (; l <= r; l += siz) {
+                cc = c - tag[pos[ll = l]], rr = l + siz - 1;
                 while (ll < rr)
-                    if (sor[mm = (ll + rr) / 2] < cc)
+                    if (sor[mm = (ll + rr) >> 1] < cc)
                         ll = mm + 1;
                     else
-                        rr = mm - 1;
-                ans += rr - i + 1;
+                        rr = mm;
+                ans += rr - l;
             }
+#ifndef ONLINE_JUDGE
+            ++cnt;
+            fscanf(ansf, "%lld", &anss);
+            if (ans != anss)
+                printf("%5d %10lld %10lld\n", cnt, anss, ans);
+#else
             printf("%lld\n", ans);
+#endif
         } else {
-            if (pos[l] == pos[l - 1]) {
-                while (l <= r && pos[l] == pos[l - 1])
+            if (pos[l - 1] == pos[l]) {
+                while (l <= r && pos[l - 1] == pos[l])
                     arr[l++] += c;
-                for (int j = l - siz; j < siz; ++j)
+                for (int j = l - siz; j < l; ++j)
                     sor[j] = arr[j];
                 std::sort(&sor[l - siz], &sor[l]);
             }
             if (pos[r] == pos[r + 1]) {
                 while (l <= r && pos[r] == pos[r + 1])
                     arr[r--] += c;
-                for (int j = r, ed = r + siz; j <= ed; ++j)
+                for (int j = r + 1; j <= r + siz; ++j)
                     sor[j] = arr[j];
-                std::sort(&sor[r], &sor[r + siz]);
+                std::sort(&sor[r + 1], &sor[r + siz + 1]);
             }
-            while (l <= r)
-                tag[pos[l]] += c, l += siz;
+            for (; l <= r; l += siz)
+                tag[pos[l]] += c;
         }
     }
 
