efd63cc0fbfbfd5e8a957273c076c5ba5606af0f
MXR612 <mxxr612@gmail.com>
2020-06-07 14:52:36 +0800

    LOJ#6285WA

--- LOJ/#6285/#6285.cpp
+++ LOJ/#6285/#6285.cpp
@@ -8,108 +8,91 @@
 #include <string.h>
 
 #include <algorithm>
-#include <set>
 #include <vector>
 
-#define MXN (100000)
-#define MXS (1000)
-
-int arr[MXN], pos[MXN] = {0};
-int cnt[MXN];
+#define MXN (100100)
+#define SIZ (55)
+#define MXB (2000)
 
 struct __lsh {
-    int u, id;
+    int u, d;
+    bool operator<(__lsh x) { return u < x.u; }
 } lsh[MXN];
-int map[MXN];
 
-bool _cmp(__lsh x, __lsh y) {
-    return x.u < y.u;
-}
-
-int pre[MXS][MXS];
+int map[MXN], arr[MXN];
 
 std::vector<int> apr[MXN];
-std::set<int> set;
+int pos[MXN] = {0};
 
-int ll, rr;
+int cnt[MXN], pre[MXB][MXB];
 
-int _cnt(int x) {
-    int ret, l, r, m;
-    for (l = 0, r = apr[x].size() - 1; l < r;)
-        if (apr[x][m = ceil((l + r) / 1.0)] <= rr)
-            l = m;
-        else
-            r = m - 1;
-    ret = r;
-    for (l = 0, r = apr[x].size() - 1; l < r;)
-        if (apr[x][m = (l + r) >> 1] < ll)
-            l = m + 1;
-        else
-            r = m;
-    return ret - r + 1;
-}
+#define que cnt
+bool vis[MXN] = {false};
 
-signed main() {
+signed
+main() {
 #ifndef ONLINE_JUDGE
     freopen("a10.in", "r", stdin);
     FILE* ansf = fopen("a10.out", "r");
-    int awsl, dif = 0;
+    int ans, dif = 0;
 #endif
 
-    int n, siz, l, r, max, maxv, cmp;
+    int n;
+    scanf("%d", &n);
 
-    scanf("%d", &n), siz = sqrt(n);
+    for (register int i = 1; i <= n; ++i)
+        scanf("%d", &lsh[i].u), lsh[i].d = i;
+    std::sort(&lsh[1], &lsh[n + 1]);
+    for (register int i = 1, p = 0; i <= n; ++i)
+        map[arr[lsh[i].d] = (lsh[i].u == lsh[i - 1].u) ? (p) : (++p)] = lsh[i].u;
 
-    for (int i = 1; i <= n; ++i) {
-        scanf("%d", &arr[i]);
-        lsh[i].u = arr[i], lsh[i].id = i;
-        pos[i] = (i - 1) / siz + 1;
-    }
+    for (register int i = 1; i <= n; ++i)
+        apr[arr[i]].push_back(i), pos[i] = (i - 1) / SIZ + 1;
     pos[n + 1] = pos[n];
 
-    std::sort(&lsh[1], &lsh[n + 1], _cmp);
-    lsh[0].u = lsh[1].u - 1;
-    for (int i = 1, p = 0; i <= n; ++i)
-        map[arr[lsh[i].id] = (lsh[i].u == lsh[i - 1].u) ? (p) : (++p)] =
-            lsh[i].u;
-    for (int i = 1; i <= n; ++i)
-        apr[arr[i]].push_back(i);
-
-    for (int i = 1; i <= n; i += siz) {
-        memset(&cnt, 0, sizeof(cnt)), max = 0;
-        for (int j = i; j <= n; j += siz) {
-            for (int k = j; k < j + siz; ++k)
-                if ((cnt[max] == (++cnt[arr[k]])) ? (arr[k] < max)
-                                                  : (cnt[max] < cnt[arr[k]]))
+    for (register int i = 1, j, k, max = 0; i <= n; i += SIZ)
+        for (memset(cnt, 0, sizeof(cnt)), j = i + SIZ - 1, k = i; j <= n; pre[pos[i]][pos[j]] = max, j += SIZ)
+            for (; k <= j; ++k)
+                if ((cnt[max] == cnt[arr[k]]) ? (arr[k] < max) : (cnt[max] < cnt[arr[k]]))
                     max = arr[k];
-            pre[pos[i]][pos[j]] = max;
-        }
-    }
 
-    for (int i = 0; i < n; ++i) {
-        scanf("%d%d", &l, &r);
-        ll = l, rr = r, set.clear();
-        while (l <= r && pos[l] == pos[l - 1])
-            set.insert(arr[l++]);
-        while (l <= r && pos[r] == pos[r + 1])
-            set.insert(arr[r--]);
-        if (l <= r)
-            set.insert(pre[pos[l]][pos[r]]);
-        maxv = 0;
-        for (std::set<int>::iterator i = set.begin(); i != set.end(); ++i)
-            if ((maxv == (cmp = _cnt(*i))) ? (*i < max) : (maxv < cmp))
-                max = *i, maxv = cmp;
+    int ll, rr, res, max, maxv = 0;
+
+    for (register int i = 0, j = 0, l, r, m, q; i < n; ++i, maxv = j = 0) {
+        scanf("%d%d", &ll, &rr), l = ll, r = rr;
+        for (; l <= r && pos[l] == pos[l - 1]; ++l)
+            if (!vis[arr[l]])
+                que[j++] = arr[l], vis[arr[l]] = true;
+        for (; l <= r && pos[r] == pos[r + 1]; --r)
+            if (!vis[arr[r]])
+                que[j++] = arr[r], vis[arr[r]] = true;
+        if (l <= r && !vis[pre[pos[l]][pos[r]]])
+            que[j++] = pre[pos[l]][pos[r]];
+        while (--j >= 0) {
+            vis[que[j]] = false;
+            for (l = 0, r = apr[q = que[j]].size() - 1; l < r;)
+                if (apr[q][m = ceil((l + r) / 2.0)] <= rr)
+                    l = m;
+                else
+                    r = m - 1;
+            for (res = l, l = 0, r = apr[q].size() - 1; l < r;)
+                if (apr[q][m = (l + r) >> 1] < ll)
+                    l = m + 1;
+                else
+                    r = m;
+            if ((maxv == (res -= r - 1)) ? (q < max) : (maxv < res))
+                max = q, maxv = res;
+        }
 #ifndef ONLINE_JUDGE
-        fscanf(ansf, "%d", &awsl);
-        if (awsl != map[max])
-            ++dif;
+        fscanf(ansf, "%d", &ans);
+        if (ans != map[max]) ++dif;
 #else
         printf("%d\n", map[max]);
 #endif
     }
 
 #ifndef ONLINE_JUDGE
-    printf("%d/%d\n", n - dif, n);
+    printf("%d/%d\n", dif, n);
 #endif
 
     return 0;
