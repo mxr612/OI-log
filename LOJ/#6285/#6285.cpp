b0b5f6df38761f292e86fc7c7bc4b4a6e08e9909
MXR612 <mxxr612@gmail.com>
2020-06-08 15:53:08 +0800

    LOJ#6285FinallyAccepted

--- LOJ/#6285/#6285.cpp
+++ LOJ/#6285/#6285.cpp
@@ -8,43 +8,60 @@
 #include <string.h>
 
 #include <algorithm>
-#include <set>
 #include <vector>
 
 #define MXN (100100)
 #define SIZ (55)
 #define MXB (2000)
 
+int n;
+
 struct __lsh {
-    int u, d;
-    bool operator<(__lsh x) { return u < x.u; }
+    int a, i;
+    bool operator<(__lsh x) { return a < x.a; }
 } lsh[MXN];
 
 int arr[MXN], map[MXN];
 
-std::vector<int> apr[MXN];
+std::vector<int> apr[MXN];  //存离散值出现的位置
 int pos[MXN] = {0};
 
-int cnt[MXN], pre[MXB][MXB];
+int cnt[MXN], pre[MXB][MXB];  //pre存block之间众数的离散值
 
-std::set<int> set;
+int ll, rr;
+
+int _cnt(int x) {
+    if (cnt[x] >= 0) return cnt[x];
+    int l, r, m, res;
+    for (l = 0, r = apr[x].size() - 1; l < r;)
+        if (apr[x][m = ceil((l + r) / 2.0)] <= rr)
+            l = m;
+        else
+            r = m - 1;
+    res = l;
+    for (l = 0, r = apr[x].size() - 1; l < r;)
+        if (apr[x][m = ceil((l + r) / 2.0)] < ll)
+            l = m;
+        else
+            r = m - 1;
+    return cnt[x] = res - l;
+}
 
 signed main() {
 #ifndef ONLINE_JUDGE
-    freopen("a10.in", "r", stdin);
-    FILE* ansf = fopen("a10.out", "r");
-    int awsl, dif = 0;
+    freopen("#6285.in", "r", stdin);
 #endif
 
-    int n;
     scanf("%d", &n);
 
     for (int i = 1; i <= n; ++i)
-        scanf("%d", &lsh[i].u), lsh[i].d = i;
-    std::sort(&lsh[0], &lsh[n + 1]);
+        scanf("%d", &lsh[i].a), lsh[i].i = i;
+    std::sort(&lsh[1], &lsh[1 + n]);
     for (int i = 1, p = 0; i <= n; ++i)
-        map[arr[lsh[i].d] = (lsh[i].u == lsh[i - 1].u) ? (p) : (++p)] = lsh[i].u;
+        map[arr[lsh[i].i] = (lsh[i].a == lsh[i - 1].a) ? (p) : (++p)] = lsh[i].a;
 
+    for (int i = 1; i <= n; ++i)
+        apr[i].push_back(0);
     for (int i = 1; i <= n; ++i)
         apr[arr[i]].push_back(i), pos[i] = (i - 1) / SIZ + 1;
     pos[n + 1] = pos[n];
@@ -52,46 +69,22 @@ signed main() {
     for (int i = 1, j, k, max = 0; i <= n; i += SIZ)
         for (memset(cnt, 0, sizeof(cnt)), j = i; j <= n; pre[pos[i]][pos[j]] = max, j += SIZ)
             for (k = j; k < j + SIZ; ++k)
-                if ((cnt[max] == cnt[arr[k]]) ? (arr[k] < max) : (cnt[max] < cnt[arr[k]]))
+                if ((cnt[max] == ++cnt[arr[k]]) ? (arr[k] < max) : (cnt[max] < cnt[arr[k]]))
                     max = arr[k];
 
-    int ll, rr, res, max, mav;
-
-    for (register int i = 1, l, r, m, x; i <= n; ++i) {
+    for (int i = 0, l, r, m, max = 0; i < n; ++i) {
         scanf("%d%d", &ll, &rr), l = ll, r = rr;
-        set.clear(), mav = 0;
+        memset(cnt, -1, sizeof(cnt));
         for (; l <= r && pos[l] == pos[l - 1]; ++l)
-            set.insert(arr[l]);
+            if ((cnt[max] == _cnt(m = arr[l])) ? (m < max) : (cnt[max] < cnt[m]))
+                max = m;
         for (; l <= r && pos[r] == pos[r + 1]; --r)
-            set.insert(arr[r]);
-        if (l <= r)
-            set.insert(pre[pos[l]][pos[r]]);
-        for (std::set<int>::iterator j = set.begin(); j != set.end(); ++j) {
-            for (l = 0, r = apr[x = *j].size() - 1; l < r;)
-                if (apr[x][m = ceil((l + r) / 2.0)] <= rr)
-                    l = m;
-                else
-                    r = m - 1;
-            for (res = l, l = 0, r = apr[x].size() - 1; l < r;)
-                if (apr[x][m = (l + r) >> 1] < ll)
-                    l = m + 1;
-                else
-                    r = m;
-            if ((mav == (res -= r - 1)) ? (x < max) : (mav < res))
-                max = x, mav = res;
-        }
-#ifndef ONLINE_JUDGE
-        fscanf(ansf, "%d", &awsl);
-        if (awsl != map[max])
-            ++dif;
-#else
+            if ((cnt[max] == _cnt(m = arr[r])) ? (m < max) : (cnt[max] < cnt[m]))
+                max = m;
+        if (l <= r && ((cnt[max] == _cnt(m = pre[pos[l]][pos[r]])) ? (m < max) : (cnt[max] < cnt[m])))
+            max = m;
         printf("%d\n", map[max]);
-#endif
     }
-
-#ifndef ONLINE_JUDGE
-    printf("%d\n", dif);
-#endif
 
     return 0;
 }
