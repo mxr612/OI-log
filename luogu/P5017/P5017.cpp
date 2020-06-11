e55fe2372227a92f38f52aaec84a842c304c84c8
MXR612 <mxxr612@gmail.com>
2020-06-11 20:26:13 +0800

    luoguP5017斜率优化练习_NOIP2019历史遗留问题

--- luogu/P5017/P5017.cpp
+++ luogu/P5017/P5017.cpp
@@ -1,43 +1,53 @@
-#include <iostream>
-#include <cstdio>
+/**
+ * NOIP2018 PJ3
+ * luogu P5017
+ * upd:把考场暴力改成斜率优化正解
+*/
 
-using namespace std;
+#include <stdio.h>
 
-int fun(int l,int r);
+#include <algorithm>
 
-int m,n;
-int sta[4000001]={0},top=-1;
-int fin[4000001],sta_mini[600],pt=0;
-int DP[4000001];
+#define MXM (200)
+#define MXT (4001000)
 
-int main(){
-	freopen("P5017.in","r",stdin);
-//InPut
-	cin>>n>>m;
-	for(int i=1,cmp;i<=n;i++){
-		cin>>cmp;
-		sta[cmp]++;
-		top=max(top,cmp);
-	}
-//Reset
-	for(int i=1;i<=top;i++)
-		if(sta[i]>0)
-			sta_mini[pt++]=i;
-	for(int i=0;i<=top;i++)
-		DP[i]=fun(0,i);
-//DP
-	for(int i=0;i<=top;i++)
-		for(int j=0;j<=i-m;j++)
-			DP[i]=min(DP[i],DP[j]+fun(j+1,i));
-//OutPut
-	cout<<DP[top];
-	return 0;
-}
+int n, m, t = 0;
+long long C[MXT] = {0};  //点数前缀和
+long long S[MXT] = {0};  //点位置前缀和
 
-int fun(int l,int r){
-	int ans=0;
-	int l_mini=0,r_mini=0;
-	for(int i=l_mini;i<=r_mini;i++)
-		ans+=sta[sta_mini[i]]*(r-sta_mini[i]);
-	return ans;
+int F[MXT] = {0};          //answer
+int Q[MXT], l = 0, r = 0;  //优先队列
+#define X(kxhfmeicgt) (C[kxhfmeicgt])
+#define Y(smvhtlapqb) (F[smvhtlapqb] + S[smvhtlapqb])
+
+int ans = 1e9;
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("P5017.in", "r", stdin);
+#endif
+
+    scanf("%d%d", &n, &m);
+
+    for (int i = 0, p; i < n; ++i)
+        scanf("%d", &p), t = std::max(t, p), ++C[p];
+    for (int i = 0; i < t + m; ++i)
+        S[i] = S[i - 1] + C[i] * i, C[i] += C[i - 1];
+
+    Q[0] = MXT - 1;
+    for (int i = 0; i < m; ++i)
+        F[i] = i * C[i] - S[i];
+    for (int i = m; i < t + m; ++i) {
+        while (l < r && (Y(i - m) - Y(Q[r])) * (X(i - m) - X(Q[r - 1])) <= (Y(i - m) - Y(Q[r - 1])) * (X(i - m) - X(Q[r]))) --r;
+        Q[++r] = i - m;
+        while (l < r && (Y(Q[l + 1]) - Y(Q[l])) <= i * ((X(Q[l + 1])) - X(Q[l]))) ++l;
+        F[i] = F[Q[l]] + i * (C[i] - C[Q[l]]) - (S[i] - S[Q[l]]);
+    }
+
+    for (int i = t; i < t + m; ++i)
+        ans = std::min(ans, F[i]);
+
+    printf("%d\n", ans);
+
+    return 0;
 }
