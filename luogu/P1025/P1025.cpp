f1005bcdc56369eec1e76f2e1ea2df0ae7a7747c
MXR612 <mxxr612@gmail.com>
2020-08-11 22:56:17 +0800

    P1025,P1631

---
+++ luogu/P1025/P1025.cpp
@@ -0,0 +1,32 @@
+/**
+ * luogu P1025
+ * 树的划分DP
+*/
+
+#define MXN (300)
+
+#include <stdio.h>
+
+int n, k;
+
+int f[MXN][MXN];
+
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("P1025.in", "r", stdin);
+#endif
+
+    scanf("%d%d", &n, &k);
+
+    f[0][0] = 1;
+    for (int i = 1, j; i <= k; ++i) {
+        for (j = 1; j <= n; ++j) {
+            f[i][j] = f[i - 1][j - 1];
+            if (j - i >= 0) f[i][j] += f[i][j - i];
+        }
+    }
+
+    printf("%d", f[k][n]);
+
+    return 0;
+}
