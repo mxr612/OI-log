a6c8ca2b75c2056bf5ee3126a27bf86f871f9058
MXR612 <mxxr612@gmail.com>
2020-08-23 08:05:05 +0800

    ACed

--- SSLOJ/2020.08.22/A/A.cpp
+++ SSLOJ/2020.08.22/A/A.cpp
@@ -9,7 +9,9 @@
 #include <algorithm>
 
 int n, a[MXN], mxa;
-long long res, ans;
+long long sum, res, ans;
+
+int t[MXN];
 
 signed main() {
 #ifndef ONLINE_JUDGE
@@ -18,14 +20,25 @@ signed main() {
 
     scanf("%d", &n);
     for (int i = 0; i < n; ++i)
-        scanf("%d", &a[i]), mxa = std::max(mxa, a[i]);
+        scanf("%d", &a[i]), sum += a[i], mxa = std::max(mxa, a[i]);
 
     if (n <= 1000 && mxa <= 2000) {  // Sub1
-        for (int i = 0; i < n; ++i)
-            ans += a[i];
+        ans = sum;
         for (int i = 2, j; i <= mxa; ++i) {
             for (res = j = 0; res < ans && j < n; ++j)
                 res += (a[j] / i) + (a[j] % i);
+            ans = std::min(ans, res);
+        }
+        printf("%lld", ans);
+    } else {
+        for (int i = 0; i < n; ++i)
+            ++t[a[i]];
+        for (int i = mxa; i >= 0; --i)
+            t[i] += t[i + 1];
+        ans = sum;
+        for (int i = 2, j; i <= mxa; ++i) {
+            for (res = sum, j = i; j <= mxa; j += i)
+                res -= (i - 1) * t[j];
             ans = std::min(ans, res);
         }
         printf("%lld", ans);
