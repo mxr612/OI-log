120c0ed50d3a7d5e996a79264ee127e248f0e9a5
MXR612 <mxxr612@gmail.com>
2020-08-31 18:13:55 +0800

    2020.08.29A

---
+++ SSLOJ/2020.08.29A/A/suffix.cpp
@@ -0,0 +1,20 @@
+/**
+ * 2020.08.29A
+ * oitiku.com/simulate-contest/1/1
+ * A 最大后缀值个数
+*/
+
+#include <stdio.h>
+
+int n, m;
+
+signed main() {
+    freopen("suffix.in", "r", stdin);
+#ifdef ONLINE_JUDGE
+    freopen("suffix.out", "w", stdin);
+#endif
+
+scanf("%d",)
+
+    return 0;
+}
