bea22b1b8d1fa6d4108c23f9d6aabb470140f8cf
mxr612 <mxxr612@gmail.com>
2020-09-09 07:44:18 +0800

    暴力和前五个点的out

---
+++ my/001/force.cpp
@@ -0,0 +1,51 @@
+
+#define ii ("05.in")
+#define oo ("05.out")
+
+#define MXN (1000020)
+
+#include <stdio.h>
+
+int n, m;
+
+long long id[MXN], arr[MXN];
+
+signed main() {
+    freopen(ii, "r", stdin);
+    freopen(oo, "w", stdout);
+
+    scanf("%d%d", &n, &m);
+
+    printf("%d %d\n", n, m);
+
+    for (long long i = 0; i < n; ++i)
+        scanf("%lld", &id[i]);
+
+    for (int i = 0; i < n; ++i)
+        scanf("%lld", &arr[i]);
+
+    int o, ans;
+    long long l, r;
+    bool is;
+    for (int i = 0, j; i < m; ++i) {
+        scanf("%d%lld%lld", &o, &l, &r);
+        if (o) {
+            is = true;
+            for (j = 0; j < n; ++j)
+                is &= (id[j] < l || r < id[j] || arr[i] > 0);
+            if (is)
+                for (j = 0; j < n; ++j)
+                    if (l <= id[j] && id[j] <= r)
+                        --arr[j];
+        } else {
+            for (j = 0; j < n; ++j)
+                if (l <= id[j] && id[j] <= r)
+                    ++arr[j];
+        }
+        for (ans = j = 0; j < n; ++j)
+            ans += (arr[j] > 0);
+        printf("%d\n", ans);
+    }
+
+    return 0;
+}
