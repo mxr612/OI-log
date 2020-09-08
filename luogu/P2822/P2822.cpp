eb0d6c0554ed242d1a1d5ad3f72b2cb46265be81
mxr612 <mxxr612@gmail.com>
2020-09-08 11:58:37 +0800

    luoguP2822

--- luogu/P2822/P2822.cpp
+++ luogu/P2822/P2822.cpp
@@ -14,29 +14,24 @@ int c[MXN][MXN], ans[MXN][MXN];
 
 int cnt(int a) {
     int res = 0;
-    while (a > 0 && a % k == 0) a /= k, ++res;
+    while (k <= a && a % k == 0) a /= k, ++res;
     return res;
 }
 
 signed main() {
 #ifndef ONLINE_JUDGE
-    freopen("P2822.in", "r", stdin);
+    freopen("P2822_3.in", "r", stdin);
 #endif
 
     scanf("%d%d", &t, &k);
 
+    c[0][0] = 1;
     for (int i = 1, j; i < MXN; ++i)
-        for (c[i][0] = cnt(1), j = 1; j <= i; ++j)
-            c[i][j] = c[i][j - 1] + cnt(i - j + 1) - cnt(j);
-    for (int i = 1; i < MXN; ++i)
-        ans[i][0] += ans[i - 1][0];
+        for (c[i][0] = 1, j = 1; j <= i; ++j)
+            c[i][j] = (c[i - 1][j] + c[i - 1][j - 1]) % k;
     for (int i = 1, j; i < MXN; ++i)
         for (j = 1; j < MXN; ++j)
-            ans[i][j] = ans[i - 1][j] + ans[i][j - 1] - ans[i - 1][j - 1] + (c[i][j] > 0);
-
-    for (int i = 0; i < 10; ++i, putchar('\n'))
-        for (int j = 0; j < 10; ++j)
-            printf("%5d", ans[i][j]);
+            ans[i][j] = ans[i - 1][j] + ans[i][j - 1] - ans[i - 1][j - 1] + (j <= i && c[i][j] == 0);
 
     for (int n, m; t-- > 0;)
         scanf("%d%d", &n, &m), printf("%d\n", ans[n][m]);
