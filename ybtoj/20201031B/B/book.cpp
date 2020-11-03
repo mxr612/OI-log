31582c15eb9b6df02c4307c74340b4885708e0de
mxr612 <m_gt_@outlook.com>
2020-11-03 09:32:45 +0800

    ybtoj20201031B

--- ybtoj/20201031B/B/book.cpp
+++ ybtoj/20201031B/B/book.cpp
@@ -2,87 +2,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include <algorithm>
 
-struct XDS{
-	int ll,rr;
-	
-	struct Node{
-		long long c,w;
-		Node*ls,*rs;
-	}*root;
-	
-	inline void merge_up(Node*x){
-		if(x){
-			x->c=x->w=0;
-			if(x->ls)x->c+=x->ls->c,x->w+=x->ls->w;
-			if(x->rs)x->c+=x->rs->c,x->w+=x->rs->w;
-		}
-	}
-	void insert(Node*&x,int l,int r,const int P,const int W){
-		if(r<l||r<P||P<l)return;
-		while(!x)x=(Node*)calloc(sizeof(Node),1);
-		if(l<r)insert(x->ls,l,(l+r)/2,P,W),insert(x->rs,(l+r)/2+1,r,P,W),merge_up(x);
-		else x->w=(x->c+=W)*l;
-	}
-	
-	int cnt(Node*x,int l,int r,const int L,const int R)const{
-		if(R<L||r<L||R<l||r<l||!x)return 0;
-		return (L<=l&&r<=R)?(x->c):(cnt(x->ls,l,(l+r)/2,L,R)+cnt(x->rs,(l+r)/2+1,r,L,R));
-	}
-	long long sum(Node*x,int l,int r,const int L,const int R)const{
-		if(R<L||r<L||R<l||r<l||!x)return 0;
-		return (L<=l&&r<=R)?(x->w):(sum(x->ls,l,(l+r)/2,L,R)+sum(x->rs,(l+r)/2+1,r,L,R));
-	}
-	
-	public:
-	
-	XDS(){
-		ll=1,rr=1e9;
-		root=NULL;
-	}
-	
-	void insert(int x,int w){insert(root,ll,rr,x,w);}
-	bool query(long long a,long long b){
-		return a*b<=b*cnt(root,ll,rr,b,rr)+sum(root,ll,rr,ll,b-1);
-	}
+const int MXN = 1000024;
+
+int arr[MXN];
+
+struct XDS {
+    int ll, rr;
+
+    struct Node {
+        long long c, w;
+        Node *ls, *rs;
+    } * root;
+
+    inline void merge_up(register Node *x) {
+        if (x) {
+            x->c = x->w = 0;
+            if (x->ls) x->c += x->ls->c, x->w += x->ls->w;
+            if (x->rs) x->c += x->rs->c, x->w += x->rs->w;
+        }
+    }
+    void insert(Node *&x, int l, int r, const int P, const int W) {
+        if (r < l || r < P || P < l) return;
+        while (!x) x = (Node *)calloc(sizeof(Node), 1);
+        if (l < r)
+            insert(x->ls, l, (l + r) / 2, P, W), insert(x->rs, (l + r) / 2 + 1, r, P, W), merge_up(x);
+        else
+            x->w = (x->c += W) * l;
+    }
+
+    int cnt(Node *x, int l, int r, const int L, const int R) const {
+        if (R < L || r < L || R < l || r < l || !x) return 0;
+        return (L <= l && r <= R) ? (x->c) : (cnt(x->ls, l, (l + r) / 2, L, R) + cnt(x->rs, (l + r) / 2 + 1, r, L, R));
+    }
+    long long sum(Node *x, int l, int r, const int L, const int R) const {
+        if (R < L || r < L || R < l || r < l || !x) return 0;
+        return (L <= l && r <= R) ? (x->w) : (sum(x->ls, l, (l + r) / 2, L, R) + sum(x->rs, (l + r) / 2 + 1, r, L, R));
+    }
+
+   public:
+    XDS() {
+        ll = 1, rr = 1e9 + 7;
+        root = NULL;
+    }
+
+    inline void insert(register int x, register int w) { insert(root, ll, rr, x, w); }
+    inline bool query(long long a, long long b) {
+        return a * b <= b * cnt(root, ll, rr, b, rr) + sum(root, ll, rr, ll, b - 1);
+    }
 };
 
-signed main(){
-	
-	freopen("book.in","r",stdin);
-	freopen("book.out","w",stdout);
-	
-	int n,m;
-	char o[5];
-	long long a,b;
-	int *arr=(int*)calloc(sizeof(int),n+32);
+char getch() {
+    register char c = getchar();
+    while (c < 'A' || 'Z' < c) c = getchar();
+    return c;
+}
+long long getll(long long &x) {
+    x = 0;
+    register char c = getchar();
+    while (c < '0' || '9' < c) c = getchar();
+    while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getchar();
+    return x;
+}
+long long getll() {
+    long long x = 0;
+    register char c = getchar();
+    while (c < '0' || '9' < c) c = getchar();
+    while ('0' <= c && c <= '9') x = x * 10 + c - '0', c = getchar();
+    return x;
+}
 
-	scanf("%d%d",&n,&m);
-	
-	if(n*m<=1e7){ // SubII 50pts
-		long long s;
-		while(m--){
-			scanf("%s%lld%lld",o,&a,&b);
-			if(!strcmp(o,"U")){
-				arr[a]=b;
-			}else{
-				s=a*b;
-				for(int i=1;a>0&&i<=n;++i)
-					s-=std::min((long long)arr[i],b);
-				printf("%s\n",(s>0)?("NIE"):("TAK"));
-			}
-		}
-	}else{
-		XDS xds;
-		while(m--){
-			scanf("%s%lld%lld",o,&a,&b);
-			if(!strcmp(o,"U"))
-				xds.insert(arr[a],-1),xds.insert(arr[a]=b,1);
-			else
-				printf("%s\n",(xds.query(a,b))?("TAK"):("NIE"));
-		}
-	}
-	
-	return 0;
+signed main() {
+#ifndef ONLINE_JUDGE
+    freopen("book.in", "r", stdin);
+    freopen("book.out", "w", stdout);
+#endif
+
+    int n, m;
+    long long a, b;
+
+    scanf("%d%d", &n, &m);
+
+    if (n * m <= 1e6) {  // SubII 50pts
+        long long s;
+        while (m--) {
+            if (getch() == 'U')
+                getll(a), getll(b), arr[a] = b;
+            else {
+                getll(a), getll(b);
+                s = a * b;
+                for (int i = 1; a > 0 && i <= n; ++i)
+                    s -= std::min((long long)arr[i], b);
+                printf("%s\n", (s > 0) ? ("NIE") : ("TAK"));
+            }
+        }
+    } else {
+        XDS xds;
+        while (m--)
+            if (getch() == 'U')
+                getll(a), getll(b), xds.insert(arr[a], -1), xds.insert(arr[a] = b, 1);
+            else
+                getll(a), getll(b), printf("%s\n", (xds.query(a, b)) ? ("TAK") : ("NIE"));
+    }
+
+    return 0;
 }
--- ybtoj/20201031B/C/work.cpp
+++ ybtoj/20201031B/C/work.cpp
@@ -1,79 +1,78 @@
 
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
+
 #include <algorithm>
-#include <math.h>
 
 const int MXN = 32,
-			MXS = 64,
-			MXT = 2097152;
-			
+          MXS = 64,
+          MXT = 1 << 21;
+
 int n;
-char S[MXN][MXS];
 
-struct Work{
-	int r,D,C;
-}work[MXN];
-
-int ans, sor[32],answ[32];
+struct Work {
+    char S[64];
+    int D, C;
+} work[MXN];
 
 // SubI
+int ans, sor[32], answ[32];
 
-void SubI(int dep,int sta,int tim,int res){
-	if(res>=ans||dep>=n){
-		if(res<ans){
-			ans=res;
-			for(int i=0;i<n;++i)
-				answ[i]=sor[i];
-		}
-	}else{
-		for(int i=0;i<n;++i)
-			if(!(sta&(1<<i)))
-				sor[dep]=i,
-				SubI(dep+1,sta|(1<<i),tim+work[i].C,res+std::max(0,tim+work[i].C-work[i].D));
-	}
+void SubI(int dep, int sta, int tim, int res) {
+    if (res >= ans || dep >= n) {
+        if (res < ans) {
+            ans = res;
+            for (int i = 0; i < n; ++i)
+                answ[i] = sor[i];
+        }
+    } else {
+        for (int i = 0; i < n; ++i)
+            if (!(sta & (1 << i)))
+                sor[dep] = i,
+                SubI(dep + 1, sta | (1 << i), tim + work[i].C, res + std::max(0, tim + work[i].C - work[i].D));
+    }
 }
 
-int ti[MXT],fr[MXT];
+int ti[MXT], fr[MXT];
 long long dp[MXT];
 
-void print(int sta){
-	if(sta)
-		print(fr[sta]),
-		printf("%s\n",S[int(log2(sta^fr[sta]))]);
-}
+signed main() {
+    freopen("example_work2.in", "r", stdin);
+    // freopen("work.out", "w", stdout);
 
-signed main(){
-	
-	freopen("work.in","r",stdin);
-	freopen("work.out","w",stdout);
-	
-	int T;
-	
-	scanf("%d",&T);
-	
-	while(T--){
-		scanf("%d",&n);
-		for(int i=0;i<n;++i)
-			scanf("%s%d%d",S[i],&work[i].D,&work[i].C),
-			work[i].r=(i>0&&!strcmp(S[i-1],S[i])?(work[i-1].r):(i));
-		if(n<=10){ // SubI 30pts
-			ans=1e9;
-			SubI(0,0,0,0);
-			printf("%lld\n",ans);
-			for(int i=0;i<n;++i)
-				printf("%s\n",S[work[answ[i]].r]);
-		}else{
-			memset(dp,0x6f,sizeof(dp)),dp[0]=0;
-			long long res;
-			for(int i=0,j,jj,k;i<(1<<n);++i)
-				for(j=0,jj=1;j<n;++j,jj<<=1)
-					if(i<(k=(i|jj))&&(res=dp[i]+std::max(0,(ti[k]=ti[i]+work[j].C)-work[j].D))<dp[k])
-						dp[k]=res,fr[k]=i;
-			printf("%lld\n",dp[(1<<n)-1]);
-			print((1<<n)-1);
-		}
-	}
-	
-	return 0;
+    int T;
+
+    scanf("%d", &T);
+
+    while (T--) {
+        scanf("%d", &n);
+        for (int i = 0; i < n; ++i)
+            scanf("%s%d%d", work[i].S, &work[i].D, &work[i].C);
+        if (n <= 10) {  // SubI 30pts
+            ans = 1e9;
+            SubI(0, 0, 0, 0);
+            printf("%lld\n", ans);
+            for (int i = 0; i < n; ++i)
+                printf("%s\n", work[answ[i]].S);
+        } else {
+            memset(dp, 0x3f, sizeof(dp)), dp[(1 << n) - 1] = 0;
+            memset(ti, 0, sizeof(ti));
+            // memset(fr, -1, sizeof(fr));
+            for (int i = 0, j; i < (1 << n); ++i)
+                for (j = 0; j < n; ++j)
+                    if (i & (1 << j))
+                        ti[i] += work[j].C;
+            long long res;
+            for (int i = (1 << n) - 1, j, k; i >= 0; --i)
+                for (j = 0, k = i | 1; j < n; k = i | (1 << (++j)))
+                    if (i < k && (res = dp[k] + std::max(0, ti[k] - work[j].D)) < dp[i])
+                        dp[i] = res, fr[i] = k;
+            printf("%lld\n", dp[0]);
+            for (int i = 0; fr[i]; i = fr[i])
+                printf("%s\n", work[int(log2(i ^ fr[i]))].S);
+        }
+    }
+
+    return 0;
 }
